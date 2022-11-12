b8e20aec6b45ed2f864554821e91c32536cdade2
Viktoriia Smaha <victoriamartinsson93@gmail.com>
2022-11-12 19:35:35 +0200

    final result

---
+++ first-home-task.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+c = int(input("Enter third number"))
+
+if a > 10 and b > 10 and c > 10:
+    if a % 3 == 0 and b % 3 == 0:
+        print("yes")
+    else:
+        print("no")
+else:
+    print("no")
